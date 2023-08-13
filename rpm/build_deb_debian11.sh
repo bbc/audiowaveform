6497093eca1bf1c0f4857763fdfdf5e8d8f1b164
Chris Needham <chris@chrisneedham.com>
2023-08-13 15:33:15 +0100

    Added scripts and Dockerfiles for building Debian packages

---
+++ rpm/build_deb_debian11.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+#
+# Create compile_audiowaveform Docker image, copy results to this directory
+# and remove the image afterwards
+
+set -e
+
+AUDIOWAVEFORM_VERSION=6bec021446bcc8f9da981158130fb200d9fc040a
+AUDIOWAVEFORM_PACKAGE_VERSION=1.8.1
+IMAGE=audiowaveform_deb
+docker build --no-cache -t $IMAGE -f Dockerfile-debian11 --build-arg AUDIOWAVEFORM_VERSION=${AUDIOWAVEFORM_VERSION} --build-arg AUDIOWAVEFORM_PACKAGE_VERSION=${AUDIOWAVEFORM_PACKAGE_VERSION} .
+CONTAINER_ID=`docker create $IMAGE`
+docker cp $CONTAINER_ID:/usr/local/src/audiowaveform-${AUDIOWAVEFORM_VERSION}/build/audiowaveform_${AUDIOWAVEFORM_PACKAGE_VERSION}-1-11_amd64.deb .
+docker rm -v $CONTAINER_ID
+docker rmi $IMAGE
