c718da4d516c5ac6b1ea792da157e3aa59bca061
ben-ganley <ben.ganley@advahealthsolutions.com>
2025-08-11 10:03:21 +1200

    Update build_all.sh to include Debian 13

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -10,6 +10,8 @@ source ./cmdline.sh
 ./build_deb_debian.sh -v ${AUDIOWAVEFORM_PACKAGE_VERSION} -c ${AUDIOWAVEFORM_VERSION} -d 11 -a arm64
 ./build_deb_debian.sh -v ${AUDIOWAVEFORM_PACKAGE_VERSION} -c ${AUDIOWAVEFORM_VERSION} -d 12 -a amd64
 ./build_deb_debian.sh -v ${AUDIOWAVEFORM_PACKAGE_VERSION} -c ${AUDIOWAVEFORM_VERSION} -d 12 -a arm64
+./build_deb_debian.sh -v ${AUDIOWAVEFORM_PACKAGE_VERSION} -c ${AUDIOWAVEFORM_VERSION} -d 13 -a amd64
+./build_deb_debian.sh -v ${AUDIOWAVEFORM_PACKAGE_VERSION} -c ${AUDIOWAVEFORM_VERSION} -d 13 -a arm64
 ./build_rpm_centos7.sh -v ${AUDIOWAVEFORM_PACKAGE_VERSION} -c ${AUDIOWAVEFORM_VERSION} -a x86_64
 ./build_rpm_centos7.sh -v ${AUDIOWAVEFORM_PACKAGE_VERSION} -c ${AUDIOWAVEFORM_VERSION} -a aarch64
 ./build_rpm_centos8.sh -v ${AUDIOWAVEFORM_PACKAGE_VERSION} -c ${AUDIOWAVEFORM_VERSION} -a x86_64
